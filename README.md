d5e0af593eabc7cd5415725c1bbff07523bf74f1
Simonas Karuzas <simonas@not.cat>
2025-10-29 17:01:00 +0200

    feat: vault view

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Intuition data migration tool
+# Intuition testnet materialized views
 
 ## Index original data
 
@@ -14,9 +14,18 @@ rindexer start indexer
 PGPASSWORD=rindexer pg_dump --dbname=postgres --host=localhost --port=5440 --username=postgres --schema=intuition_multi_vault --schema-only --file=schema.sql
 ```
 
+## Dump data
+
+```
+PGPASSWORD=rindexer pg_dump --dbname=postgres --host=localhost --port=5440 --username=postgres --schema=intuition_multi_vault --data-only --file=data.sql
+```
+
+
 ## Import migrations 
 
 ```
 PGPASSWORD=rindexer psql -h localhost -p 5440 -U postgres -d postgres -f migrations/position.sql
+PGPASSWORD=rindexer psql -h localhost -p 5440 -U postgres -d postgres -f migrations/vault.sql
+
 ```
 ```
--- mock.sql
+++ mock.sql
@@ -8,8 +8,28 @@ CREATE TABLE "public"."position" (
     "total_redeem_assets_for_receiver" numeric(78,0) NOT NULL DEFAULT 0,
     "updated_at" timestamptz NOT NULL DEFAULT now(),
     "created_at" timestamptz NOT NULL DEFAULT now(),
+  block_number BIGINT NOT NULL,
+  log_index BIGINT NOT NULL,
+  transaction_hash TEXT NOT NULL,
+  transaction_index BIGINT NOT NULL,
     PRIMARY KEY ("account_id", "term_id", "curve_id")
 );
 
 
+
+CREATE TABLE IF NOT EXISTS vault (
+  term_id TEXT NOT NULL,
+  curve_id NUMERIC(78, 0) NOT NULL,
+  total_shares NUMERIC(78, 0) NOT NULL,
+  current_share_price NUMERIC(78, 0) NOT NULL,
+  total_assets NUMERIC(78, 0) NOT NULL DEFAULT 0,
+  market_cap NUMERIC(78, 0) NOT NULL DEFAULT 0,
+  position_count INTEGER NOT NULL,
+  block_number BIGINT NOT NULL,
+  log_index BIGINT NOT NULL,
+  transaction_hash TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  PRIMARY KEY (term_id, curve_id)
+);
 
